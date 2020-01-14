fd24d2f9be20c54242a04328e1e630c8b91a2fbc
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-14 18:25:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 class Formatter {
-    //add static methods here
-    static capitalize (string) {
-      return string.charAt(0).toUpperCase();  
-
-    }
-    static sanitize () {}
-    static titleize () {}
+  //add static methods here
+  static capitalize (str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
   }
-
-let formatter = new Formatter('crocodile')
-Formatter.capitalize();
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9 '-]/g, '')  
+  }
+  static titleize(str){
+    let bannedWords = ['the','a','an','but','of','and','for','at','by','from'];
+    let strArr = str.split(' ');
+    let newArr =[];
+    for(let i=0;i < strArr.length;i++){
+      if(i === 0){
+       newArr.push(this.capitalize(strArr[i]));
+      }
+      else{
+        if(bannedWords.includes(strArr[i])){
+          newArr.push(strArr[i])
+        }
+        else{
+          newArr.push(this.capitalize(strArr[i]))
+        }
+      }
+    }
+    return newArr.join(' ');
+  }
+  
+}
